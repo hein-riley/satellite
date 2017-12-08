1dc85ac2c57925ccfb3e4bcb52263e1ba81722b7
hein-riley <rjhein3@gmail.com>
2017-12-07 20:56:30 -0700

    this time for sure

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+</body>	
+	
+<footer class="teminal-foot container-fluid footer-class">
+
+	<div class="container wrapper">
+		<div class="footer">	
+			<p>All rights reserved. Content &copy; <?php echo date('Y'); ?> Riley Hein </p>
+
+		</div><!--footer-->
+	</div>
+
+</footer>
+
+</html>
